ede89fb29297da375a08c563948077c6b84e557b
Dmytro Morozov <dmytro.morozov@cariad.technology>
2023-06-20 10:07:25 +0200

    Init

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿using Grpc.Net.Client;
+using GrpcClient;
+
+using var channel = GrpcChannel.ForAddress("https://dimigrpc.azurewebsites.net");
+var client = new Greeter.GreeterClient(channel);
+var reply = await client.SayHelloAsync(new HelloRequest { Name = "GreeterClient" });
+Console.WriteLine($"Greeting: {reply.Message}");
+Console.Read();
