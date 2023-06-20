f4a61930f2159ae92698a5ac40bc6384746fb4b9
Dmytro Morozov <dmytro.morozov@cariad.technology>
2023-06-20 17:21:26 +0200

    Add debug

--- Program.cs
+++ Program.cs
@@ -1,7 +1,10 @@
 ﻿using Grpc.Net.Client;
 using GrpcClient;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
 
-using var channel = GrpcChannel.ForAddress("https://dimigrpc.azurewebsites.net");
+var loggerFactory = LoggerFactory.Create(builder => builder.AddFilter("*", LogLevel.Debug).AddConsole());
+using var channel = GrpcChannel.ForAddress("https://dimigrpc.azurewebsites.net", new() { LoggerFactory = loggerFactory });
 var client = new Greeter.GreeterClient(channel);
 var reply = await client.SayHelloAsync(new HelloRequest { Name = "GreeterClient" });
 Console.WriteLine($"Greeting: {reply.Message}");
